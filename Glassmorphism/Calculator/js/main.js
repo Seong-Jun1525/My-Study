1726b90252c7dfcaad64f5e828e3e5badf383fa8
Seong-Jun1525 <sjsj123455@naver.com>
2021-09-29 21:41:58 +0900

    G-4 complete

---
+++ Glassmorphism/Calculator/js/main.js
@@ -0,0 +1,6 @@
+VanillaTilt.init(document.querySelector(".container"), {
+    max: 15,
+    speed: 400,
+    glare: true,
+    "max-glare": 0.2 // the maximum "glare" opacity (1 = 100%, 0.5 = 50%)
+});
